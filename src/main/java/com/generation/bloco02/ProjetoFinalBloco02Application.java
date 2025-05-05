92a42b19dfaedcbb201ab861eada4dd08b44a815
Henrique Machado <henriquesousa@ymail.com>
2025-05-05 09:18:19 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/generation/bloco02/ProjetoFinalBloco02Application.java
@@ -0,0 +1,13 @@
+package com.generation.bloco02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBloco02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBloco02Application.class, args);
+	}
+
+}
